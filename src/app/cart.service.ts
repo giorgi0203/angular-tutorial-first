6712828cd738c4478fecacd6bc648fdb299f2818
giorgi <giorgi1997arabuli@gmail.com>
2019-07-05 18:53:40 +0400

    feat(3-managing-data): Finish tutorial

---
+++ src/app/cart.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items = [];
+
+  constructor() {
+  }
+
+  addToCart(product: any) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+
+    return this.items;
+  }
+}
