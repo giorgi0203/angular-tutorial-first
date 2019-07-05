c26ddbe9ce79fe28de3722618ebc5ffc4561d48a
giorgi <giorgi1997arabuli@gmail.com>
2019-07-05 20:02:06 +0400

    feat(5-form-basics): Finish tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {
         path: '', component: ProductListComponent,
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,13 +10,25 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
     this.items = this.cartService.getItems();
     console.log(this.cartService);
 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      adress: ''
+    });
+  }
+
+  onSubmit(costumerData) {
+    console.warn('order has been submited ', costumerData);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
   ngOnInit() {
