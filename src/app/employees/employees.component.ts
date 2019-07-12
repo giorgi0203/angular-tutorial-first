b5578e5934f2cfdb4072a4bc7b98cf544e6f92c0
giorgi <giorgi1997arabuli@gmail.com>
2019-07-12 12:34:33 -0700

    feat(Http): Add custom navigation

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -9,6 +9,10 @@ import { EmployeesService } from '../employees.service';
 export class EmployeesComponent implements OnInit {
 
   employees$;
+  pager;
+  rowsCount = 10;
+  pagedItems;
+  allItems;
 
   constructor(
     private employeesService: EmployeesService
@@ -16,11 +20,67 @@ export class EmployeesComponent implements OnInit {
 
   ngOnInit() {
     this.employees$ = this.employeesService.getEmplyees();
+
+    this.employees$.subscribe(data=>{
+      this.allItems = data;
+      this.setPage(1);
+    });
   }
+
+  setPage(page: number) {
+
+        this.pager = this.getPager(this.allItems.length, page);
+
+        this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    }
 
   remove(id) {
     this.employeesService.deleteEmplyee(id).subscribe(data => console.log(data));
     this.employees$ = this.employeesService.getEmplyees();
   }
+
+  getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10) {
+        let totalPages = Math.ceil(totalItems / pageSize);
+
+        if (currentPage < 1) { 
+            currentPage = 1; 
+        } else if (currentPage > totalPages) { 
+            currentPage = totalPages; 
+        }
+        
+        let startPage: number, endPage: number;
+        if (totalPages <= 10) {
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+
+        let startIndex = (currentPage - 1) * pageSize;
+        let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+        let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
+    }
 
 }
