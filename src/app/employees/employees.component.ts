c4468038a98439807214951b4a363dd58fefa72a
GiorgiArabuli <giorgi1997arabuli@gmail.com>
2019-07-15 13:48:02 +0400

    fix(Employees): Fix ts lints

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -21,7 +21,7 @@ export class EmployeesComponent implements OnInit {
   ngOnInit() {
     this.employees$ = this.employeesService.getEmplyees();
 
-    this.employees$.subscribe(data=>{
+    this.employees$.subscribe(data => {
       this.allItems = data;
       this.setPage(1);
     });
@@ -29,10 +29,10 @@ export class EmployeesComponent implements OnInit {
 
   setPage(page: number) {
 
-        this.pager = this.getPager(this.allItems.length, page);
+    this.pager = this.getPager(this.allItems.length, page);
 
-        this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    }
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
 
   remove(id) {
     this.employeesService.deleteEmplyee(id).subscribe(data => console.log(data));
@@ -40,47 +40,48 @@ export class EmployeesComponent implements OnInit {
   }
 
   getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10) {
-        let totalPages = Math.ceil(totalItems / pageSize);
+    const totalPages = Math.ceil(totalItems / pageSize);
 
-        if (currentPage < 1) { 
-            currentPage = 1; 
-        } else if (currentPage > totalPages) { 
-            currentPage = totalPages; 
-        }
-        
-        let startPage: number, endPage: number;
-        if (totalPages <= 10) {
-            startPage = 1;
-            endPage = totalPages;
-        } else {
-            if (currentPage <= 6) {
-                startPage = 1;
-                endPage = 10;
-            } else if (currentPage + 4 >= totalPages) {
-                startPage = totalPages - 9;
-                endPage = totalPages;
-            } else {
-                startPage = currentPage - 5;
-                endPage = currentPage + 4;
-            }
-        }
-
-        let startIndex = (currentPage - 1) * pageSize;
-        let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-        let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
-
-        return {
-            totalItems: totalItems,
-            currentPage: currentPage,
-            pageSize: pageSize,
-            totalPages: totalPages,
-            startPage: startPage,
-            endPage: endPage,
-            startIndex: startIndex,
-            endIndex: endIndex,
-            pages: pages
-        };
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > totalPages) {
+      currentPage = totalPages;
     }
+
+    let startPage: number;
+    let endPage: number;
+    if (totalPages <= 10) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+    const pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+
+    return {
+      totalItems,
+      currentPage,
+      pageSize,
+      totalPages,
+      startPage,
+      endPage,
+      startIndex,
+      endIndex,
+      pages
+    };
+  }
 
 }
