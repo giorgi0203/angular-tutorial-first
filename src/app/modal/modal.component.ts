7250d59fb7c1e88533f491372b746ab05948237e
giorgi <giorgi1997arabuli@gmail.com>
2019-07-16 18:19:29 +0400

    feat(Animation): Update practice modal animation

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ModalService } from '../modal.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [
+    trigger('openCloseModal', [
+      state('in', style({ height: '100%' })),
+
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({  height: '0%', }),
+        animate(500)
+      ]),
+
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate(500, style({ height: '0%', })))
+    ]),
+    trigger('openCloseFade', [
+      state('in', style({ opacity: .5 })),
+
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({ opacity: 0, }),
+        animate(500)
+      ]),
+
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate(500, style({ opacity: 0 })))
+    ])
+  ]
 })
 export class ModalComponent implements OnInit, OnDestroy {
 
